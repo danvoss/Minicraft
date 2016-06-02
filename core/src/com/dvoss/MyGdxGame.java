8a19970d8f09e624e2887a789e3da9ca0613a45f
Dan <danielandrewvoss@gmail.com>
2016-06-02 13:57:26 -0400

    Add functionality

--- core/src/com/dvoss/MyGdxGame.java
+++ core/src/com/dvoss/MyGdxGame.java
@@ -2,6 +2,7 @@ package com.dvoss;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
@@ -11,6 +12,13 @@ import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureRegion down, up, right, left;
+	float x, y, xv, yv;
+
+	static final float MAX_VELOCITY = 100;
+	static final float DECELERATION = 0.95f;
+
+	static final int WIDTH = 18;
+	static final int HEIGHT = 26;
 	
 	@Override
 	public void create () {
@@ -27,10 +35,42 @@ public class MyGdxGame extends ApplicationAdapter {
 	@Override
 	public void render () {
 
+		move();
+
 		Gdx.gl.glClearColor(0, 1, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(right, 1, 1);
+		batch.draw(right, x, y, WIDTH * 3, HEIGHT * 3);
 		batch.end();
+	}
+
+	public void move() {
+		if (Gdx.input.isKeyPressed(Input.Keys.UP)) {
+			yv = MAX_VELOCITY;
+		}
+		else if (Gdx.input.isKeyPressed(Input.Keys.DOWN)) {
+			yv = -MAX_VELOCITY;
+		}
+		if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			xv = MAX_VELOCITY;
+		}
+		else if (Gdx.input.isKeyPressed((Input.Keys.LEFT))) {
+			xv = -MAX_VELOCITY;
+		}
+
+		float delta = Gdx.graphics.getDeltaTime();
+		y += yv * delta;
+		x += xv * delta;
+
+		yv = decelerate(yv);
+		xv = decelerate(xv);
+	}
+
+	public float decelerate(float velocity) {
+		velocity *= DECELERATION;
+		if (Math.abs(velocity) < 1) {
+			velocity = 0;
+		}
+		return velocity;
 	}
 }
