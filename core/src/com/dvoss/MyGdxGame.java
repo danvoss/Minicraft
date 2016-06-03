66453d7bf18a48a8e0b3bbbbfc4dff9649a4955a
Dan <danielandrewvoss@gmail.com>
2016-06-03 10:43:30 -0400

    Add stuff

--- core/src/com/dvoss/MyGdxGame.java
+++ core/src/com/dvoss/MyGdxGame.java
@@ -7,24 +7,26 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
-import com.sun.xml.internal.ws.policy.privateutil.PolicyUtils;
+
+import java.util.Random;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
 	TextureRegion down, up, stand, right, left, tree, zombie;
-	float x, y, xv, yv;
+	float x, y, xv, yv, zombiex, zombiey, zombxv, zombyv;
+
 
 	static final float MAX_VELOCITY = 100;
 	static final float DECELERATION = 0.5f;
 
-	static final int WIDTH = 18;
-	static final int HEIGHT = 26;
+	static final int WIDTH = 16;
+	static final int HEIGHT = 16;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		Texture tiles = new Texture("tiles.png");
-		TextureRegion[][] treeGrid = TextureRegion.split(tiles, 18, 12);
+		TextureRegion[][] treeGrid = TextureRegion.split(tiles, WIDTH/2, HEIGHT/2);
 		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
 		down = grid[6][0];
 		up = grid[6][1];
@@ -33,6 +35,8 @@ public class MyGdxGame extends ApplicationAdapter {
 		left = new TextureRegion(right);
 		left.flip(true, false);
 		tree = treeGrid[1][0];
+		tree.setRegionWidth(WIDTH);
+		tree.setRegionHeight(HEIGHT);
 		zombie = grid[6][7];
 	}
 
@@ -70,19 +74,35 @@ public class MyGdxGame extends ApplicationAdapter {
 			y = 0;
 		}
 
+		moveZombie();
+
+		if (zombiex < 0) {
+			zombiex = 600;
+		}
+		if (zombiex > 600) {
+			zombiex = 0;
+		}
+		if (zombiey < 0) {
+			zombiey = 600;
+		}
+		if (zombiey > 600) {
+			zombiey = 0;
+		}
+
+
 		Gdx.gl.glClearColor(0, 1, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, x, y, WIDTH * 2, HEIGHT * 2);
-		batch.draw(zombie, 500, 500, 30, 30);
-		batch.draw(tree, 300, 300, 30, 30);
-		batch.draw(tree, 195, 453, 35, 35);
-		batch.draw(tree, 547, 234, 32, 32);
-		batch.draw(tree, 222, 123, 28, 28);
-		batch.draw(tree, 432, 564, 30, 30);
-		batch.draw(tree, 100, 80, 30, 30);
-		batch.draw(tree, 80, 300, 26, 26);
-		batch.draw(tree, 520, 80, 28, 28);
+		batch.draw(zombie, zombiex, zombiey, WIDTH * 2, HEIGHT * 2);
+		batch.draw(tree, 300, 300, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 195, 453, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 547, 234, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 222, 123, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 432, 564, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 100, 80, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 80, 300, WIDTH * 1.5f, HEIGHT * 1.5f);
+		batch.draw(tree, 520, 80, WIDTH * 1.5f, HEIGHT * 1.5f);
 		batch.end();
 	}
 
@@ -128,5 +148,25 @@ public class MyGdxGame extends ApplicationAdapter {
 			velocity = 0;
 		}
 		return velocity;
+	}
+	public void moveZombie() {
+
+		float delta = Gdx.graphics.getDeltaTime();
+		zombiey += zombyv * delta;
+		zombiex += zombxv * delta;
+
+		Random random = new Random();
+		if (random.nextBoolean() == true) {
+			zombyv = MAX_VELOCITY;
+		}
+		else if (random.nextBoolean() == false) {
+			zombyv = -MAX_VELOCITY;
+		}
+		if (random.nextBoolean() == true) {
+			zombxv = MAX_VELOCITY;
+		}
+		else if (random.nextBoolean() == false) {
+			zombxv = -MAX_VELOCITY;
+		}
 	}
 }
