7404222026b9bfd1a78cdd73de3d2a67e45b3319
Dan <danielandrewvoss@gmail.com>
2016-06-03 15:34:37 -0400

    Add sounds and jelly

--- core/src/com/dvoss/MyGdxGame.java
+++ core/src/com/dvoss/MyGdxGame.java
@@ -15,8 +15,8 @@ import static com.badlogic.gdx.Gdx.input;
 
 public class MyGdxGame extends ApplicationAdapter {
 	SpriteBatch batch;
-	TextureRegion down, up, stand, right, left, tree, zombie_u, zombie_d, zombie_r, zombie_l, zombie_s;
-	float x, y, xv, yv, zom_x, zom_y, zom_xv, zom_yv;
+	TextureRegion down, up, stand, right, left, tree, zombie_u, zombie_d, zombie_r, zombie_l, zombie_s, jelly1, jelly2;
+	float x, y, xv, yv, zom_x, zom_y, zom_xv, zom_yv, jel_y, jel_x, jel_yv, jel_xv;
 
 
 	static final float MAX_VELOCITY = 100;
@@ -29,7 +29,7 @@ public class MyGdxGame extends ApplicationAdapter {
 	public void create () {
 		batch = new SpriteBatch();
 		Texture tiles = new Texture("tiles.png");
-		TextureRegion[][] treeGrid = TextureRegion.split(tiles, WIDTH/2, HEIGHT/2);
+		TextureRegion[][] treeGrid = TextureRegion.split(tiles, WIDTH / 2, HEIGHT / 2);
 		TextureRegion[][] grid = TextureRegion.split(tiles, 16, 16);
 		down = grid[6][0];
 		up = grid[6][1];
@@ -45,7 +45,9 @@ public class MyGdxGame extends ApplicationAdapter {
 		zombie_d = grid[6][4];
 		zombie_r = grid[6][7];
 		zombie_l = new TextureRegion(zombie_r);
-		zombie_l.flip(true,false);
+		zombie_l.flip(true, false);
+		jelly1 = grid[7][4];
+		jelly2 = grid[7][5];
 	}
 
 	@Override
@@ -112,16 +114,61 @@ public class MyGdxGame extends ApplicationAdapter {
 			zom_y = 0;
 		}
 
-		Sound sound1 = Gdx.audio.newSound(Gdx.files.internal("bossdeath.wav"));
+		moveJelly();
+
+		TextureRegion j_img;
+		if (jel_yv > 0) {
+			j_img = jelly1;
+		}
+		else {
+			j_img = jelly2;
+		}
+		if (jel_xv > 0) {
+			j_img = jelly2;
+		}
+		else {
+			j_img = jelly1;
+		}
+
+		if (jel_x < 0) {
+			jel_x = 600;
+		}
+		if (jel_x > 600) {
+			jel_x = 0;
+		}
+		if (jel_y < 0) {
+			jel_y = 600;
+		}
+		if (jel_y > 600) {
+			jel_y = 0;
+		}
+
+		Sound sound1 = Gdx.audio.newSound(Gdx.files.internal("death.wav"));
 		if ((Math.abs(x - zom_x) < 10) && (Math.abs(y - zom_y) < 10)) {
 			sound1.play(1.0f);
 		}
+
+		Sound sound2 = Gdx.audio.newSound(Gdx.files.internal("pickup.wav"));
+		if ((Math.abs(x - 300) < 8) && (Math.abs(y - 300) < 8) || (Math.abs(x - 195) < 8) && (Math.abs(y - 453) < 8)
+				|| (Math.abs(x - 547) < 8) && (Math.abs(y - 234) < 8) || (Math.abs(x - 222) < 8) && (Math.abs(y - 123)
+				< 8) || (Math.abs(x - 432) < 8) && (Math.abs(y - 564) < 8) || (Math.abs(x - 100) < 8) && (Math.abs(y
+				- 80) < 8) || (Math.abs(x - 80) < 8) && (Math.abs(y - 300) < 8) || (Math.abs(x - 520) < 8) &&
+				(Math.abs(y - 80) < 8)) {
+			sound2.play(1.0f);
+		}
+
+		Sound sound3 = Gdx.audio.newSound(Gdx.files.internal("playerhurt.wav"));
+		if ((Math.abs(x - jel_x) < 10) && (Math.abs(y - jel_y) < 10)) {
+			sound3.play(1.0f);
+		}
+
 
 		Gdx.gl.glClearColor(0, 1, 0.4f, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
 		batch.draw(img, x, y, WIDTH * 2, HEIGHT * 2);
 		batch.draw(z_img, zom_x, zom_y, WIDTH * 2, HEIGHT * 2);
+		batch.draw(j_img, jel_x, jel_y, WIDTH * 2, HEIGHT * 2);
 		batch.draw(tree, 300, 300, WIDTH * 1.5f, HEIGHT * 1.5f);
 		batch.draw(tree, 195, 453, WIDTH * 1.5f, HEIGHT * 1.5f);
 		batch.draw(tree, 547, 234, WIDTH * 1.5f, HEIGHT * 1.5f);
@@ -176,6 +223,7 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 		return velocity;
 	}
+
 	public void moveZombie() {
 
 		float delta = Gdx.graphics.getDeltaTime();
@@ -195,6 +243,28 @@ public class MyGdxGame extends ApplicationAdapter {
 		}
 		else if (!random.nextBoolean()) {
 			zom_xv = -MAX_VELOCITY/1.5f;
+		}
+	}
+
+	public void moveJelly() {
+
+		float delta = Gdx.graphics.getDeltaTime();
+		jel_y += jel_yv * delta;
+		jel_x += jel_xv * delta;
+
+		Random random = new Random();
+
+		if (random.nextBoolean()) {
+			jel_yv = MAX_VELOCITY;
+		}
+		else if (!random.nextBoolean()) {
+			jel_yv = -MAX_VELOCITY;
+		}
+		if (random.nextBoolean()) {
+			jel_xv = MAX_VELOCITY;
+		}
+		else if (!random.nextBoolean()) {
+			jel_xv = -MAX_VELOCITY;
 		}
 	}
 }
